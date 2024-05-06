ad2e897aa3da19db781bb3d2b26a432e37cbe3c1
YouXam <youxam@outlook.com>
2024-05-06 10:20:10 +0800

    Update lastCommitMessage variable to only show the commit subject

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { execSync } from "child_process";
 
 const commitDate = execSync('git log -1 --format=%cI').toString().trimEnd();
 const commitHash = execSync('git rev-parse HEAD').toString().trimEnd();
-const lastCommitMessage = execSync('git show -s --format="%an: %s"').toString().trimEnd();
+const lastCommitMessage = execSync('git show -s --format="%s"').toString().trimEnd();
 
 process.env.VITE_GIT_COMMIT_DATE = commitDate;
 process.env.VITE_GIT_COMMIT_HASH = commitHash;
