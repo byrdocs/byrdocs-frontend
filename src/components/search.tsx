84ccb0615031637adee97df2af29821b3adf610f
YouXam <youxam@outlook.com>
2025-05-04 03:49:59 +0800

    Fix: always loading

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -238,7 +238,7 @@ export function Search({ onPreview: onLayoutPreview }: { onPreview: (preview: bo
                                     </div>)}
 
                                     <div className="absolute left-[15px] top-1/2 transform -translate-y-1/2 w-6 h-6 text-muted-foreground">
-                                        {suspenseLoading || loading || debouncing || miniSearching ?
+                                        {top && suspenseLoading || loading || debouncing || miniSearching ?
                                             <svg className="animate-spin text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
