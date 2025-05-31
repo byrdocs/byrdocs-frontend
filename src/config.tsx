b7204e631f9e03644d3a1a02be98dbdff33114b4
YouXam <youxam@outlook.com>
2025-05-31 23:45:52 +0800

    Fix banner text color in dark mode

--- src/config.tsx
+++ src/config.tsx
@@ -7,7 +7,7 @@ const GITHUB_URL = "https://github.com/byrdocs/byrdocs-archive"
 export function Banner({ className }: { className?: string }) {
     return (
         <div className={cn("w-full text-sm bg-secondary text-muted-foreground flex flex-row flex-wrap justify-center py-2", className)}>
-            <Link to={GITHUB_URL} target="_blank" className="text-primary/70 hover:underline">
+            <Link to={GITHUB_URL} target="_blank" className="text-primary/70 dark:text-primary hover:underline">
                 如果 BYR Docs 有帮助，Star 我们的 GitHub 仓库！
             </Link>
             <div className="ml-1 flex items-center">
