c6f6a0bc72300d02fa3cb8a99a7d5714cbe41179
YouXam <youxam@outlook.com>
2025-06-22 16:48:44 +0800

    fix github login

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -35,9 +35,8 @@ export default function Callback() {
                     if (service === 'byrdocs' && res.data) {
                         window.location.href = res.data
                     }
-                    if (res.redirect) {
-                        window.location.href = res.redirect
-                    }
+                } else if (res.redirect) {
+                    window.location.href = res.redirect
                 } else {
                     setError(res.error);
                 }
